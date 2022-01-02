9d44f238ed1775d058a8bc2be030e1e108899c1e
mtkrated <57570477+mtkrated@users.noreply.github.com>
2022-01-02 21:11:58 +0200

    Add functions to return video information and links

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,3 +1,8 @@
+const hd = 720;
+const mid = 480;
+const low = 360;
+const superLow = 240;
+
 const isValidUrl = (url) => {
   return url.includes(process.env.URL);
 };
@@ -9,7 +14,77 @@ const getJsonUrl = (url) => {
   return first + ".json" + second;
 };
 
+const getVideoInfo = (json) => {
+  return {
+    video_url:
+      json[0].data.children[0].data.secure_media.reddit_video.fallback_url,
+    thumbnail: json[0].data.children[0].data.thumbnail,
+    thumbnail_height: json[0].data.children[0].data.thumbnail_height,
+    thumbnail_width: json[0].data.children[0].data.thumbnail_width,
+    title: json[0].data.children[0].data.title,
+    height: json[0].data.children[0].data.secure_media.reddit_video.height,
+  };
+};
+
+const getVideoSound = (json) => {
+  const video_url =
+    json[0].data.children[0].data.secure_media.reddit_video.fallback_url;
+  const height = json[0].data.children[0].data.secure_media.reddit_video.height;
+  return video_url.replace(`DASH_${height}`, "DASH_audio");
+};
+
+const getDifferentVideoQualities = (json) => {
+  const video_url =
+    json[0].data.children[0].data.secure_media.reddit_video.fallback_url;
+  const height = json[0].data.children[0].data.secure_media.reddit_video.height;
+  const videoQuality = `DASH_${height}`;
+  switch (height) {
+    case 1080: {
+      return {
+        uhd: video_url,
+        hd: video_url.replace(videoQuality, changeVideoQuality(hd)),
+        480: video_url.replace(videoQuality, changeVideoQuality(mid)),
+        360: video_url.replace(videoQuality, changeVideoQuality(low)),
+        240: video_url.replace(videoQuality, changeVideoQuality(superLow)),
+      };
+    }
+    case 720: {
+      return {
+        hd: video_url,
+        480: video_url.replace(videoQuality, changeVideoQuality(mid)),
+        360: video_url.replace(videoQuality, changeVideoQuality(low)),
+        240: video_url.replace(videoQuality, changeVideoQuality(superLow)),
+      };
+    }
+    case 480: {
+      return {
+        480: video_url,
+        360: video_url.replace(videoQuality, changeVideoQuality(low)),
+        240: video_url.replace(videoQuality, changeVideoQuality(superLow)),
+      };
+    }
+    case 360: {
+      return {
+        360: video_url,
+        240: video_url.replace(videoQuality, changeVideoQuality(superLow)),
+      };
+    }
+    default: {
+      return {
+        video: video_url,
+      };
+    }
+  }
+};
+
+const changeVideoQuality = (height) => {
+  return `DASH_${height}`;
+};
+
 module.exports = {
   isValidUrl,
   getJsonUrl,
+  getVideoSound,
+  getVideoInfo,
+  getDifferentVideoQualities,
 };
--- src/routes/linkroutes.js
+++ src/routes/linkroutes.js
@@ -6,16 +6,21 @@ const linkRouter = express.Router();
 
 linkRouter.post("/", async (req, res) => {
   data = req.body.url;
+  let json;
 
   if (util.isValidUrl(data)) {
     const newUrl = util.getJsonUrl(data);
     const resp = await fetch(newUrl);
-    const json = await resp.json();
-
-    res.send(
-      `<img src="${json[0].data.children[0].data.thumbnail}" width="250" height="250"/>`
-    );
+    json = await resp.json();
+  } else {
+    res.status(400);
   }
+
+  res.send({
+    ...util.getVideoInfo(json),
+    ...util.getDifferentVideoQualities(json),
+    sound: util.getVideoSound(json),
+  });
 });
 
 module.exports = linkRouter;
