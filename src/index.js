df9508e721716a984e0960a4483cdd3f06bfb3da
Motheo Keneilwe <93276929+madnyte@users.noreply.github.com>
2022-01-13 12:53:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/link", require("./routes/linkroutes"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
